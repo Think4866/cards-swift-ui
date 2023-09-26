75996cb37747b3ea663b95132a437059043bc9eb
Nathaniel Bennett <ntbnnt@users.noreply.github.com>
2023-09-26 02:20:23 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Lopen on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
